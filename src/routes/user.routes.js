f81bc737c59304e9aa66bfab490b9ae40df47a28
Jhonny Freitas <jhonnyfreitas348@gmail.com>
2023-05-30 12:25:45 -0300

    Created avatar upload

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,19 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
-
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticaded = require("../middlewares/ensureAuthenticaded");
 
 const userRoutes = Router();
-
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 userRoutes.post("/", usersController.create);
 userRoutes.put("/", ensureAuthenticaded, usersController.update);
+userRoutes.patch("/avatar", ensureAuthenticaded, upload.single("avatar"), userAvatarController.update)
 
 module.exports = userRoutes;
